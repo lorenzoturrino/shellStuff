58dcd4a4892225ef2ed96a3e42bf4b6516b74678
lorenzoturrino <lorenzo.turrino@gmail.com>
2016-09-07 10:14:52 +0100

    backup alias and senv function

---
+++ alias.sh
@@ -0,0 +1,15 @@
+# python and django
+function senv {
+  source ${1:-env}/bin/activate
+}
+alias denv='deactivate'
+alias rserv='manage.py runserver'
+alias mama='manage.py makemigrations'
+alias mami='manage.py migrate'
+
+# mysql i hate you
+alias my='mysql -p'
+
+# git stuff
+alias glp='git pull origin `git rev-parse --abbrev-ref HEAD`'
+alias gup='git push origin `git rev-parse --abbrev-ref HEAD`'
